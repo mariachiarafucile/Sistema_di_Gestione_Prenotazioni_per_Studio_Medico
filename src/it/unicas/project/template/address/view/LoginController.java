ff9dbbf5e535f3c64f302975e3ebde6a83cbc99e
alessia0603 <alessia.scittarelli@studentmail.unicas.it>
2025-12-03 18:38:43 +0100

    Running version

---
+++ src/it/unicas/project/template/address/view/LoginController.java
@@ -0,0 +1,98 @@
+package it.unicas.project.template.address.view;
+
+import it.unicas.project.template.address.MainApp;
+import it.unicas.project.template.address.model.Medici;
+import it.unicas.project.template.address.model.Segretari;
+import it.unicas.project.template.address.model.dao.DAO;
+import it.unicas.project.template.address.model.dao.DAOException;
+import it.unicas.project.template.address.model.dao.mysql.MediciDAOMySQLImpl;
+import it.unicas.project.template.address.model.dao.mysql.SegretariDAOMySQLImpl;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.util.Optional;
+
+public class LoginController {
+
+    @FXML
+    private Label titleLabel;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+
+    private MainApp mainApp;
+    private String loginMode; // "MEDICO" o "SEGRETARIO"
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    /**
+     * Imposta se siamo nel login Medico o Segretario.
+     */
+    public void setLoginMode(String loginMode) {
+        this.loginMode = loginMode;
+
+        // if (titleLabel != null) {
+        titleLabel.setText("Login ");
+        //  }
+    }
+
+    @FXML
+    private void onLogin() throws DAOException {
+        String user = usernameField.getText();
+        String pass = passwordField.getText();
+
+        System.out.println("Tentativo login (" + loginMode + "): "
+                + user + " / " + pass);
+
+        if (user.isEmpty() || pass.isEmpty()) {
+            showErrorAlert();
+            return;
+        }
+
+        if (loginMode.equals("MEDICO")) {
+            DAO<Medici> dao = MediciDAOMySQLImpl.getInstance();
+            boolean valid = ((MediciDAOMySQLImpl) dao).existsMedico(user, pass);
+
+            if (!valid) {
+                showErrorAlert();
+                return;
+            }
+            mainApp.showMedicoDashboard();
+        }
+        if (loginMode.equals("SEGRETARIO")) {
+            DAO<Segretari> dao = SegretariDAOMySQLImpl.getInstance();
+            boolean valid = ((SegretariDAOMySQLImpl) dao).existsSegretario(user, pass);
+
+            if (!valid) {
+                showErrorAlert();
+                return;
+               }
+            mainApp.showSegretarioDashboard();
+            }
+        }
+
+
+    @FXML
+    private void onRegister() {
+
+        if (loginMode.equals("MEDICO")) {
+            mainApp.showRegistrazioneMedico();
+        } else {
+            mainApp.showRegistrazioneSegretario();
+        }
+    }
+
+    @FXML
+    private void onBack() {
+        mainApp.showIdentificazione();
+    }
+
+    public void showErrorAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setContentText("Credenziali non valide. Riprova.");
+        alert.showAndWait();
+    }
+}
