40dea4f3232902f6d29511ff32381c930bc8a4d0
denisedomenicone <denise.domenicone@studentmail.unicas.it>
2025-12-03 12:25:21 +0100

    Running version

---
+++ src/it/unicas/project/template/address/model/FasceOrarie.java
@@ -0,0 +1,85 @@
+package it.unicas.project.template.address.model;
+
+import javafx.beans.property.*;
+
+public class FasceOrarie {
+
+    private IntegerProperty idFasciaOraria;
+    private StringProperty data;
+    private StringProperty oraInizio;
+    private StringProperty oraFine;
+
+    public FasceOrarie() {
+        this(null, "", "", "");
+    }
+
+    public FasceOrarie(Integer idFasciaOraria, String data, String oraInizio, String oraFine) {
+        this.idFasciaOraria = new SimpleIntegerProperty(idFasciaOraria);
+        this.data = new SimpleStringProperty(data);
+        this.oraInizio = new SimpleStringProperty(oraInizio);
+        this.oraFine = new SimpleStringProperty(oraFine);
+    }
+
+    public Integer getIdFasciaOraria() {
+        if (idFasciaOraria == null) {
+            idFasciaOraria = new SimpleIntegerProperty(-1);
+        }
+        return idFasciaOraria.get();
+    }
+
+    public void setIdFasciaOraria(Integer idFasciaOraria) {
+        if (this.idFasciaOraria == null) {
+            this.idFasciaOraria = new SimpleIntegerProperty();
+        }
+        this.idFasciaOraria.set(idFasciaOraria);
+    }
+
+    public IntegerProperty idFasciaOrariaProperty() {
+        return idFasciaOraria;
+    }
+
+    public String getData() {
+        return data.get();
+    }
+
+    public void setData(String data) {
+        this.data.set(data);
+    }
+
+    public StringProperty dataProperty() {
+        return data;
+    }
+
+    public String getOraInizio() {
+        return oraInizio.get();
+    }
+
+    public void setOraInizio(String oraInizio) {
+        this.oraInizio.set(oraInizio);
+    }
+
+    public StringProperty oraInizioProperty() {
+        return oraInizio;
+    }
+
+    public String getOraFine() {
+        return oraFine.get();
+    }
+
+    public void setOraFine(String oraFine) {
+        this.oraFine.set(oraFine);
+    }
+
+    public StringProperty oraFineProperty() {
+        return oraFine;
+    }
+
+    @Override
+    public String toString() {
+        return idFasciaOraria.getValue() + ", " +
+                data.getValue() + ", " +
+                oraInizio.getValue() + ", " +
+                oraFine.getValue();
+    }
+}
+
