8b2642e945103aebde8632bf12335e0b0c2b2c30
mariachiarafucile <mariachiara.fucile@studentmail.unicas.it>
2025-12-05 12:05:33 +0100

    Running version

--- src/it/unicas/project/template/address/view/RicercaPazienteController.java
+++ src/it/unicas/project/template/address/view/RicercaPazienteController.java
@@ -1,9 +1,11 @@
 package it.unicas.project.template.address.view;
 
 import it.unicas.project.template.address.MainApp;
+import it.unicas.project.template.address.model.AlertUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class RicercaPazienteController {
@@ -21,6 +23,25 @@ public class RicercaPazienteController {
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
+    }
+
+    @FXML
+    private void initialize() {
+        // Quando la scena è pronta, allora carichiamo il logo
+        cfField.sceneProperty().addListener((obs, oldScene, newScene) -> {
+            if (newScene != null) {
+                newScene.windowProperty().addListener((obs2, oldWindow, newWindow) -> {
+                    if (newWindow != null) {
+                        try {
+                            Image logo = new Image(AlertUtils.class.getResourceAsStream("/images/logo.png"));
+                            ((Stage) newWindow).getIcons().add(logo);
+                        } catch (Exception e) {
+                            System.out.println("Logo non trovato");
+                        }
+                    }
+                });
+            }
+        });
     }
 
     public void setDialogStage(Stage stage) {
