115a9812b4b54d96dcbc51dad342dd41d0500dfd
mariachiarafucile <mariachiara.fucile@studentmail.unicas.it>
2025-12-11 10:45:41 +0100

    Running version

--- src/it/unicas/project/template/address/view/ReportController.java
+++ src/it/unicas/project/template/address/view/ReportController.java
@@ -8,6 +8,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import it.unicas.project.template.address.model.dao.mysql.PagamentiDAOMySQLImpl;
+import it.unicas.project.template.address.model.dao.DAOException;
+
 
 public class ReportController {
 
@@ -31,6 +34,14 @@ public class ReportController {
 
     private Stage dialogStage;
     private MainApp mainApp;
+
+    // Campi per salvare i valori correnti del report
+    private int currentVisitePagate = 0;
+    private int currentVisiteDaSaldare = 0;
+    private double currentTotaleIncassato = 0.0;
+    private double currentTotaleDaIncassare = 0.0;
+    private String currentMese = "";
+
 
     @FXML
     private void initialize() {
@@ -78,11 +89,60 @@ public class ReportController {
                 "Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre"
         );
         mesePicker.setPromptText("Seleziona mese");
+
+        mesePicker.getSelectionModel().selectedIndexProperty().addListener((obs, oldIndex, newIndex) -> {
+            if (newIndex != null && newIndex.intValue() >= 0) {
+                aggiornaReport(newIndex.intValue() + 1); // Convertiamo l'indice (0-11) in mese (1-12)
+            }
+        });
+
     }
 
+    private void aggiornaReport(int mese) {
+        try {
+            PagamentiDAOMySQLImpl.ReportResult r = ((PagamentiDAOMySQLImpl) PagamentiDAOMySQLImpl.getInstance()).getReportByMonth(mese);
+
+            visitePagateLabel.setText("Numero visite pagate: " + r.visitePagate);
+            visiteDaSaldareLabel.setText("Numero visite da saldare: " + r.visiteDaSaldare);
+            totaleIncassatoLabel.setText("Totale incassato: " + r.totaleIncassato + " €");
+            totaleDaIncassareLabel.setText("Totale da incassare: " + r.totaleDaIncassare + " €");
+
+            // --- Aggiorna campi per CSV ---
+            currentVisitePagate = r.visitePagate;
+            currentVisiteDaSaldare = r.visiteDaSaldare;
+            currentTotaleIncassato = r.totaleIncassato;
+            currentTotaleDaIncassare = r.totaleDaIncassare;
+            currentMese = mesePicker.getValue();
+
+            System.out.println("Report aggiornato per il mese: " + mese);
+        } catch (DAOException e) {
+            e.printStackTrace();
+            System.out.println("Errore nel generare il report: " + e.getMessage());
+        }
+
+    }
+
+    // --- Metodo per salvare il report in CSV ---
     private void scaricaReport() {
-        System.out.println("Scarica report mese: " + (mesePicker.getValue() != null ? mesePicker.getValue() : "non selezionato"));
-        // Logica per generare/esportare il report
+        if (currentMese.isEmpty()) {
+            System.out.println("Seleziona un mese prima di scaricare il report.");
+            return;
+        }
+
+        String fileName = "Report_" + currentMese + ".csv";
+
+        try (java.io.PrintWriter writer = new java.io.PrintWriter(fileName)) {
+            writer.println("Mese,Visite Pagate,Totale Incassato,Visite Da Saldare,Totale Da Incassare");
+            writer.println(currentMese + "," +
+                    currentVisitePagate + "," +
+                    currentTotaleIncassato + "," +
+                    currentVisiteDaSaldare + "," +
+                    currentTotaleDaIncassare);
+
+            System.out.println("Report salvato in: " + fileName);
+        } catch (Exception e) {
+            System.out.println("Errore nel salvare il report: " + e.getMessage());
+        }
     }
 
     public void setDialogStage(Stage dialogStage) {
