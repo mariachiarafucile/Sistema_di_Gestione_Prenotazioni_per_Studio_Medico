dd943733637d1764cda629170769f885b350e427
mariachiarafucile <mariachiara.fucile@studentmail.unicas.it>
2025-12-15 13:05:37 +0100

    Running version

--- src/it/unicas/project/template/address/model/dao/mysql/PrenotazioniDAOMySQLImpl.java
+++ src/it/unicas/project/template/address/model/dao/mysql/PrenotazioniDAOMySQLImpl.java
@@ -13,12 +13,13 @@ import java.util.logging.Logger;
 
 public class PrenotazioniDAOMySQLImpl implements DAO<Prenotazioni> {
 
-    private static DAO dao = null;
     private static Logger logger = null;
 
     private PrenotazioniDAOMySQLImpl(){}
 
-    public static DAO getInstance() {
+    private static PrenotazioniDAOMySQLImpl dao = null;
+
+    public static PrenotazioniDAOMySQLImpl getInstance() {
         if (dao == null) {
             dao = new PrenotazioniDAOMySQLImpl();
             logger = Logger.getLogger(PrenotazioniDAOMySQLImpl.class.getName());
@@ -159,6 +160,39 @@ public class PrenotazioniDAOMySQLImpl implements DAO<Prenotazioni> {
             throw new DAOException("In executeUpdate(): " + e.getMessage());
         }
     }
+
+    public List<Prenotazioni> selectPrenotazioniScadute() throws DAOException {
+
+        List<Prenotazioni> lista = new ArrayList<>();
+
+        String sql =
+                "SELECT p.* " +
+                        "FROM prenotazioni p " +
+                        "JOIN fasceOrarie f ON p.fasciaOrariaId = f.idFasciaOraria " +
+                        "WHERE STR_TO_DATE(CONCAT(f.data, ' ', f.oraFine), '%Y-%m-%d %H:%i') < NOW()";
+
+        try {
+            Statement st = DAOMySQLSettings.getStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+                lista.add(new Prenotazioni(
+                        rs.getInt("idPrenotazioni"),
+                        rs.getString("paziente_codiceFiscale"),
+                        rs.getString("medico_email"),
+                        rs.getInt("fasciaOrariaId")
+                ));
+            }
+
+            DAOMySQLSettings.closeStatement(st);
+
+        } catch (SQLException e) {
+            throw new DAOException("Errore selectPrenotazioniScadute: " + e.getMessage());
+        }
+
+        return lista;
+    }
+
 }
 
 
